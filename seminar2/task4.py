bf62f6e86dc06c3597c7b91774ed420386789fff
OzodbekUSTECH <naimovozod81@gmail.com>
2024-10-03 11:06:17 +0300

    seminar 2, task 4

---
+++ seminar2/task4.py
@@ -0,0 +1,8 @@
+# name is a parameter of the function hello_user
+def hello_user(name: str):
+    print(f"Hello, {name}!")
+
+
+# Run the function
+# "Alice" is an argument of the function hello_user
+hello_user("Alice")
